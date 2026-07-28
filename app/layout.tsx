7d829ac26663a84e24f3f13ba70637b340af9221
Corpore <corpore.saojose@gmail.com>
2026-07-28 16:27:32 -0300

    fix: adiciona snippet base do Meta Pixel (fbq init + PageView) ausente no layout
    O window.fbq estava undefined em producao pois o script fbevents.js
    e o fbq('init', ...) nunca foram adicionados ao layout, so existia a
    chamada window.fbq('track','Lead') em page.tsx sem base para executar.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Archivo, Archivo_Black } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
+
+const META_PIXEL_ID = "1841018156120390";
 
 const archivo = Archivo({
   subsets: ["latin"],
@@ -28,6 +31,37 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pt-BR" className={`${archivo.variable} ${archivoBlack.variable}`}>
+      <head>
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            if (!window.__metaPixelInitialized) {
+              window.__metaPixelInitialized = true;
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+
+              var pvEventId = (window.crypto && crypto.randomUUID)
+                ? crypto.randomUUID()
+                : ('pv_' + Date.now() + '_' + Math.random().toString(36).slice(2, 9));
+
+              fbq('init', '${META_PIXEL_ID}');
+              fbq('track', 'PageView', {}, { eventID: pvEventId });
+            }
+          `}
+        </Script>
+        <noscript>
+          <img
+            height="1" width="1" style={{ display: "none" }}
+            src={`https://www.facebook.com/tr?id=${META_PIXEL_ID}&ev=PageView&noscript=1`}
+            alt=""
+          />
+        </noscript>
+      </head>
       <body className="font-sans bg-ink-900 text-[#faf8f0] antialiased">
         {children}
       </body>
